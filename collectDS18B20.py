abc7460af0114608c879e3a4d6f0548feaa7711b
Kyle Evans <k.evans@gmx.com>
2014-04-28 08:49:21 -0400

    changed the pull in a little bit still would like error handling but it works now

--- collectDS18B20.py
+++ collectDS18B20.py
@@ -6,6 +6,9 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 def readsensor(input):
-    raw = open(input, "r").read()
-    temperature = float(raw.split("t=")[-1])/1000
-    return round(temperature, 1)
+    tempfile = open(input, "r").read()
+    thetext = tempfile.read()
+    tempdata = thetext.split("\n")[1].split(" ")[9]
+    temperature = float(tempdata[2:])
+    temperature = temperature / 1000
+    return temperature
